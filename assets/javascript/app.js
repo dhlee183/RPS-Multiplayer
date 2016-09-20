0ec0c1cef68383e5b48367d2142a8ae2452248e0
David Hyun Lee <David Hyun Lee>
2016-09-19 21:31:26 -0400

    Created the basic html and css design

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAJQdfisWqzBhO3Uhy5zA_1LpY66jLCT14",
+	authDomain: "rps-multiplayer-73e86.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-73e86.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "787730844177"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
